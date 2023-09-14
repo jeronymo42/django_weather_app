0c33b11a12ca4f1d381d51b0f4bfda0105d7a687
V.Ledovskikh <vladimir.ledovskikh@gmail.com>
2023-09-14 13:51:10 +0300

    add ip geolocation and wether forecast request

---
+++ main/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, HttpResponse
+from .api_config import DATA_TOKEN, WEATHER_TOKEN
+import requests, json
+
+# Create your views here.
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+def index(request):
+    # Getting client's IP adress
+    ip = get_client_ip(request)
+
+    # Requesting client's city by IP from API
+    params = {"token": DATA_TOKEN}
+    city = requests.get(f'https://ipinfo.io/{ip}', params=params).json()['city']
+
+    # Requesting weather forecast for specific city for 3 days
+    querystring = {"q":city,"days":"3"}
+    headers = {
+        "X-RapidAPI-Key": WEATHER_TOKEN,
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+    }
+    response = requests.get("https://weatherapi-com.p.rapidapi.com/forecast.json", headers=headers, params=querystring)
+    return HttpResponse(response)
--- weather_app/settings.py
+++ weather_app/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-from ..config import KEY
+from .config import KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -117,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media files (Music, Video)
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
